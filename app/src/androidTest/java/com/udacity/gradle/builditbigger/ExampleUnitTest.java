cd9e401e1fa2ffd95c4aeaff748f2c71a209d259
downs990 <downscourtney17@gmail.com>
2021-03-14 21:04:06 -0400

    finished testing the AsyncTask non-empty string

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleUnitTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ExampleUnitTest.java
@@ -1,6 +1,29 @@
 package com.udacity.gradle.builditbigger;
 
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+
 public class ExampleUnitTest {
+
+
+    @Test
+    public void testVerifyEchoResponse() {
+
+        EndpointsAsyncTask endpointTask = new EndpointsAsyncTask(
+                new EndpointsAsyncTask.OnCompletionListener() {
+                    @Override
+                    public void onComplete(String joke) {
+                        // Test to make sure joke is a non-empty string.
+                        assertNotNull(joke);
+
+                    }
+                });
+
+//        endpointTask.execute(new Pair<Context, String>(context, "Manfred"));
+        endpointTask.execute();
+
+    }
 
 
 
